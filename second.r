78caad03cb18ccbf6b6514bed8e77a1f84dbfea4
Maimoon <102782444+Maimo0n@users.noreply.github.com>
2022-05-25 10:40:46 +0500

    second.r
    Third file on GitHub. And this is my print command! ?/

---
+++ second.r
@@ -0,0 +1,21 @@
+# This repository file responced by me. 
+#    NOW !!!
+#   The shortcut of print ,save ,copy ,paste are follows:
+
+#    Ctrl + P is for print
+#    Ctrl + S is for save 
+#    Ctrl + C is for copy
+#    Ctrl + V is for paste
+
+
+x = 4
+z = 9
+
+if x > z :
+    print("This ecuation is false")
+else:
+    print("OK! Fineee Alright")
+    
+    
+    
+    
